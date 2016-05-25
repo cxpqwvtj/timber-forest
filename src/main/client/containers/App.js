9648b306d67a6476b8e443fd6172e35850e24f71
cxpqwvtj <cxpqwvtj@gmail.com>
2016-05-26 07:18:09 +0900

    reduxのreal-worldから必要なファイルを持ってきた

---
+++ src/main/client/containers/App.js
@@ -0,0 +1,81 @@
+import React, { Component, PropTypes } from 'react'
+import { connect } from 'react-redux'
+import { browserHistory } from 'react-router'
+import { resetErrorMessage } from '../actions'
+
+import { MuiThemeProvider, getMuiTheme } from 'material-ui/styles'
+import { AppBar } from 'material-ui'
+
+import injectTapEventPlugin from 'react-tap-event-plugin'
+injectTapEventPlugin()
+
+class App extends Component {
+  constructor(props) {
+    super(props)
+    this.handleChange = this.handleChange.bind(this)
+    this.handleDismissClick = this.handleDismissClick.bind(this)
+  }
+
+  handleDismissClick(e) {
+    this.props.resetErrorMessage()
+    e.preventDefault()
+  }
+
+  handleChange(nextValue) {
+    browserHistory.push(`/${nextValue}`)
+  }
+
+  renderErrorMessage() {
+    const { errorMessage } = this.props
+    if (!errorMessage) {
+      return null
+    }
+
+    return (
+      <p style={{ backgroundColor: '#e99', padding: 10 }}>
+        <b>{errorMessage}</b>
+        {' '}
+        (<a href="#"
+            onClick={this.handleDismissClick}>
+          Dismiss
+        </a>)
+      </p>
+    )
+  }
+
+  render() {
+    const { children } = this.props
+    return (
+      <div>
+        <MuiThemeProvider muiTheme={getMuiTheme()}>
+          <AppBar
+            title="timber-forest"
+            iconClassNameRight="muidocs-icon-navigation-expand-more"
+          />
+        </MuiThemeProvider>
+        {children}
+        {this.renderErrorMessage()}
+      </div>
+    )
+  }
+}
+
+App.propTypes = {
+  // Injected by React Redux
+  errorMessage: PropTypes.string,
+  resetErrorMessage: PropTypes.func.isRequired,
+  inputValue: PropTypes.string.isRequired,
+  // Injected by React Router
+  children: PropTypes.node
+}
+
+function mapStateToProps(state, ownProps) {
+  return {
+    errorMessage: state.errorMessage,
+    inputValue: ownProps.location.pathname.substring(1)
+  }
+}
+
+export default connect(mapStateToProps, {
+  resetErrorMessage
+})(App)
--- src/main/client/index.js
+++ src/main/client/index.js
@@ -1,8 +1,17 @@
 import 'babel-polyfill'
 import React from 'react'
 import { render } from 'react-dom'
+import { browserHistory } from 'react-router'
+import { syncHistoryWithStore } from 'react-router-redux'
+import Root from './containers/Root'
+import configureStore from './store/configureStore'
+
+const store = configureStore()
+const history = syncHistoryWithStore(browserHistory, store)
 
 render(
-  <div>Hello World</div>,
+  <div>
+    <Root store={store} history={history} />
+  </div>,
   document.getElementById('root')
 )
