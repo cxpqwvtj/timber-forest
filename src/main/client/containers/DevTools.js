0db9ad3d4c260e4d5986e0d22363d3487660dfcb
cxpqwvtj <cxpqwvtj@gmail.com>
2016-05-28 10:00:19 +0900

    redux-devtools-chart-monitor追加

--- src/main/client/containers/DevTools.js
+++ src/main/client/containers/DevTools.js
@@ -2,10 +2,11 @@ import React from 'react'
 import { createDevTools } from 'redux-devtools'
 import LogMonitor from 'redux-devtools-log-monitor'
 import DockMonitor from 'redux-devtools-dock-monitor'
+import ChartMonitor from 'redux-devtools-chart-monitor'
 
 export default createDevTools(
-  <DockMonitor toggleVisibilityKey="ctrl-h"
-               changePositionKey="ctrl-w">
+  <DockMonitor toggleVisibilityKey="ctrl-h" changePositionKey="ctrl-w" changeMonitorKey="ctrl-m" defaultIsVisible={false}>
     <LogMonitor />
+    <ChartMonitor />
   </DockMonitor>
 )
