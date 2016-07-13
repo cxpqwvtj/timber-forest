53f7ff5d7c2c96cbfed2bc0463dfc91f484b5a73
cxpqwvtj <cxpqwvtj@gmail.com>
2016-07-13 22:52:37 +0900

    コンテキストパスの指定を調整

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,35 @@
 var webpack = require('webpack')
 var HtmlWebpackPlugin = require('html-webpack-plugin')
 
+const DEBUG = !process.argv.includes('--release')
+const VERBOSE = process.argv.includes('--verbose')
+let CONTEXT_PATH = `${(process.env.CONTEXT_PATH || '')}`
+
 module.exports = {
   context: __dirname + '/src/main/client',
   entry: {
-    'js/bundle': ['webpack-hot-middleware/client', './index.js']
+    'js/bundle': [...(DEBUG ? ['webpack-hot-middleware/client'] : []), './index.js']
   },
   output: {
     path: __dirname + '/src/main/resources/static',
     filename: '[name].js',
-    publicPath: '/'
+    publicPath: `${CONTEXT_PATH}/`
   },
   resolve: {
     extensions: ['', '.js', '.jsx']
   },
-  devtool: 'cheap-module-eval-source-map',
+  devtool: DEBUG ? 'cheap-module-eval-source-map' : false,
   plugins: [
-    new webpack.optimize.OccurenceOrderPlugin(),
-    new webpack.HotModuleReplacementPlugin(),
+    new webpack.DefinePlugin({
+      'process.env.CONTEXT_PATH': `"${CONTEXT_PATH}"`,
+      'process.env.NODE_ENV': `"${process.env.NODE_ENV || (DEBUG ? 'development' : 'production')}"` 
+    }),
+    ...(DEBUG ? [new webpack.HotModuleReplacementPlugin()] : [
+      new webpack.optimize.OccurenceOrderPlugin(),
+      new webpack.optimize.DedupePlugin(),
+      new webpack.optimize.AggressiveMergingPlugin(),
+      new webpack.optimize.UglifyJsPlugin({ compress: { screw_ie8: true, warnings: VERBOSE } })
+    ]),
     new HtmlWebpackPlugin({
       title: 'timber-forest',
       template: 'index.ejs'
