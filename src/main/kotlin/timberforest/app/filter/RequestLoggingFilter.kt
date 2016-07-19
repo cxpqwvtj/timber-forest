845e17c654ad9ad6816e3b0043dc56ad294aa200
cxpqwvtj <cxpqwvtj@gmail.com>
2016-07-20 06:48:13 +0900

    リクエストの詳細をtraceレベルで出力するように修正

--- src/main/kotlin/timberforest/app/filter/RequestLoggingFilter.kt
+++ src/main/kotlin/timberforest/app/filter/RequestLoggingFilter.kt
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpServletRequest
 /**
  * Created by masahiro on 2016/04/14.
  */
-class RequestLoggingFilter: Filter {
+class RequestLoggingFilter : Filter {
 
     private val logger = LoggerFactory.getLogger(RequestLoggingFilter::class.java)
     private val LF = "\n"
@@ -42,33 +42,21 @@ class RequestLoggingFilter: Filter {
                 map.put("content-length", request.contentLengthLong)
             }
             logger.trace("[REQUEST]" + ObjectMapper().writeValueAsString(map))
-            val requestInfo = mutableListOf<String>()
-            requestInfo.add("${request.javaClass.name}")
-            requestInfo.add("${request.requestedSessionId}")
-            requestInfo.add("${request.requestURL}")
-            requestInfo.add("${request.queryString}")
-            requestInfo.add("${request.method}")
-            requestInfo.add("${request.protocol}")
-            requestInfo.add("${request.scheme}")
-            requestInfo.add("${request.isSecure}")
-            requestInfo.add("${request.remoteAddr}")
-            requestInfo.add("${request.remoteHost}")
-            requestInfo.add("${request.characterEncoding}")
-            requestInfo.add("${request.contentLength}")
-            requestInfo.add("${request.contentType}")
-            requestInfo.add("${request.locale}")
-            requestInfo.add("${request.locales}")
 
             val list = mutableListOf<String>()
-            list.add("*********************** Begin$LF")
-            list.add(request.headerNames.toList().map { "[header]$it: ${request.getHeader(it)}" }.joinToString(LF))
-            list.add(request.parameterNames.toList().map { name -> "[$name]${request.getParameterValues(name).joinToString(", ")}" }.joinToString(LF))
-//            request.cookies
-//            request.attributeNames
+            list.add("*********************** Begin")
+            list.add("[URL]${request.requestURL} [query]${request.queryString} [SessionId]${request.requestedSessionId} [class]${request.javaClass.name}")
+            list.add("[method]${request.method} [protocol]${request.protocol} [scheme]${request.scheme} [secure]${request.isSecure} [RemoteAddr]${request.remoteAddr} [RemoteHost]${request.remoteHost}")
+            list.add("[encoding]${request.characterEncoding} [ContentLength]${request.contentLength} [contentType]${request.contentType} [locale]${request.locale} [locales]${request.locales.toList().joinToString(" ")}")
+            list.addAll(request.headerNames?.toList()?.map { "[header]$it: ${request.getHeader(it)}" } ?: arrayListOf())
+            list.addAll(request.parameterNames?.toList()?.map { "[parameter]$it: ${request.getParameterValues(it).joinToString(", ")}" } ?: arrayListOf())
+            list.addAll(request.cookies?.toList()?.map { "[cookie]${it.name}: ${it.value}" } ?: arrayListOf())
+            list.addAll(request.attributeNames?.toList()?.map { "[attribute]$it: ${request.getAttribute(it)}" } ?: arrayListOf())
             val session = request.getSession(false)
-            if (session!=null){
-//                session.attributeNames
+            if (session != null) {
+                list.addAll(session.attributeNames?.toList()?.map { "[session]$it: ${session.getAttribute(it)}" } ?: arrayListOf())
             }
+            logger.trace(list.joinToString(LF))
         }
         chain.doFilter(servletRequest, servletResponse)
     }
