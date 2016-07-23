fd502096ba64d95416c37646111ad7504059a238
cxpqwvtj <cxpqwvtj@gmail.com>
2016-07-24 07:37:47 +0900

    ログレベルによって出力内容変更するように調整

--- src/main/kotlin/timberforest/app/filter/RequestLoggingFilter.kt
+++ src/main/kotlin/timberforest/app/filter/RequestLoggingFilter.kt
@@ -1,9 +1,7 @@
 package timberforest.app.filter
 
-import com.fasterxml.jackson.databind.ObjectMapper
 import org.slf4j.LoggerFactory
 import java.io.IOException
-import java.util.*
 import javax.servlet.*
 import javax.servlet.http.HttpServletRequest
 
@@ -22,25 +20,29 @@ class RequestLoggingFilter : Filter {
 
     @Throws(IOException::class, ServletException::class)
     override fun doFilter(servletRequest: ServletRequest, servletResponse: ServletResponse, chain: FilterChain) {
-        if (logger.isTraceEnabled) {
+        if (logger.isDebugEnabled) {
             val request = servletRequest as HttpServletRequest
             val list = mutableListOf<String>()
             list.add("*********************** BEGIN")
-            list.add("[URL]${request.requestURL} [query]${request.queryString} [SessionId]${request.requestedSessionId} [class]${request.javaClass.name}")
-            list.add("[method]${request.method} [protocol]${request.protocol} [scheme]${request.scheme} [secure]${request.isSecure} [RemoteAddr]${request.remoteAddr} [RemoteHost]${request.remoteHost}")
-            list.add("[encoding]${request.characterEncoding} [ContentLength]${request.contentLength} [contentType]${request.contentType} [locale]${request.locale} [locales]${request.locales.toList().joinToString(" ")}")
-            list.addAll(request.headerNames?.toList()?.map { "[header]$it: ${request.getHeader(it)}" } ?: arrayListOf())
-            list.addAll(request.parameterNames?.toList()?.map { "[parameter]$it: ${request.getParameterValues(it).joinToString(", ")}" } ?: arrayListOf())
-            list.addAll(request.cookies?.toList()?.map { "[cookie]${it.name}: ${it.value}" } ?: arrayListOf())
-            list.addAll(request.attributeNames?.toList()?.map { "[attribute]$it: ${request.getAttribute(it)}" } ?: arrayListOf())
-            val session = request.getSession(false)
-            if (session != null) {
-                list.addAll(session.attributeNames?.toList()?.map { "[session]$it: ${session.getAttribute(it)}" } ?: arrayListOf())
+            list.add("[URL]${request.requestURL} [query]${request.queryString} [method]${request.method}")
+            if (logger.isTraceEnabled) {
+                list.add("[protocol]${request.protocol} [scheme]${request.scheme} [secure]${request.isSecure} [RemoteAddr]${request.remoteAddr} [RemoteHost]${request.remoteHost} [SessionId]${request.requestedSessionId} [class]${request.javaClass.name}")
+                list.add("[ContentLength]${request.contentLength} [contentType]${request.contentType} [encoding]${request.characterEncoding} [locale]${request.locale} [locales]${request.locales.toList().joinToString(" ")}")
+                list.addAll(request.headerNames?.toList()?.map { "[header]$it: ${request.getHeader(it)}" } ?: arrayListOf())
+                list.addAll(request.parameterNames?.toList()?.map { "[parameter]$it: ${request.getParameterValues(it).joinToString(", ")}" } ?: arrayListOf())
+                list.addAll(request.cookies?.toList()?.map { "[cookie]${it.name}: ${it.value}" } ?: arrayListOf())
+                list.addAll(request.attributeNames?.toList()?.map { "[attribute]$it: ${request.getAttribute(it)}" } ?: arrayListOf())
+                val session = request.getSession(false)
+                if (session != null) {
+                    list.addAll(session.attributeNames?.toList()?.map { "[session]$it: ${session.getAttribute(it)}" } ?: arrayListOf())
+                }
             }
-            logger.trace(list.joinToString(LF))
+            logger.debug(list.joinToString(LF))
         }
         chain.doFilter(servletRequest, servletResponse)
-        logger.trace("*********************** END$LF")
+        if (logger.isDebugEnabled) {
+            logger.debug("*********************** END$LF")
+        }
     }
 
     override fun destroy() {
