d565dee7afcd13c7ce1c84bd44444e1a1ded247e
cxpqwvtj <cxpqwvtj@gmail.com>
2016-09-25 07:25:31 +0900

    不要なimport削除

--- src/main/kotlin/timberforest/app/config/WebAppConfig.kt
+++ src/main/kotlin/timberforest/app/config/WebAppConfig.kt
@@ -1,12 +1,8 @@
 package timberforest.app.config
 
-import org.springframework.boot.autoconfigure.web.WebMvcAutoConfiguration
-import org.springframework.boot.context.embedded.FilterRegistrationBean
-import org.springframework.context.annotation.Bean
 import org.springframework.stereotype.Component
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
-import timberforest.app.filter.RequestLoggingFilter
 import timberforest.app.interceptor.RequestLoggingInterceptor
 
 /**
