efb637de88108ddc3ca35a06a61a0aff61c3a593
cxpqwvtj <cxpqwvtj@gmail.com>
2016-09-30 07:02:38 +0900

    インターセプタのログ出力処理実装

--- src/main/kotlin/timberforest/app/aspect/TraceInterceptor.kt
+++ src/main/kotlin/timberforest/app/aspect/TraceInterceptor.kt
@@ -1,7 +1,28 @@
 package timberforest.app.aspect
 
+import org.aspectj.lang.ProceedingJoinPoint
+import org.aspectj.lang.annotation.Around
+import org.aspectj.lang.annotation.Aspect
+import org.slf4j.LoggerFactory
+import org.springframework.stereotype.Component
+
 /**
  * Created by masahiro on 2016/09/30.
  */
-open class TraceInterceptor {
+@Component
+@Aspect
+class TraceInterceptor {
+    private val logger = LoggerFactory.getLogger(this.javaClass)
+
+    @Around("")
+    fun invoke(proceedingJoinPoint: ProceedingJoinPoint): Any {
+        var ret: Any? = null
+        try {
+            logger.trace("[BEGIN]${proceedingJoinPoint.target.javaClass.name}#${proceedingJoinPoint.signature.name} ${proceedingJoinPoint.args}")
+            ret = proceedingJoinPoint.proceed()
+            return ret
+        } finally {
+            logger.trace("[ END ]${proceedingJoinPoint.target.javaClass.name}#${proceedingJoinPoint.signature.name} ${ret}")
+        }
+    }
 }
